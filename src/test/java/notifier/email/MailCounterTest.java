78eeb800d884bd8c3d189b296898be380a01b538
Adam Mihoubi <adam.mihoubi-ext@fnacdarty.com>
2026-03-17 14:00:28 +0100

    refacto test

--- src/test/java/notifier/email/MailCounterTest.java
+++ src/test/java/notifier/email/MailCounterTest.java
@@ -1,11 +1,11 @@
 package notifier.email;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import notifier.doubles.StubTimeProvider;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MailCounterTest {
     private final StubTimeProvider timeProvider = new StubTimeProvider();
@@ -15,22 +15,20 @@ class MailCounterTest {
     void should_be_limited_if_it_has_been_triggered_more_than_a_hundred_time_in_an_hour() {
         final var startTime = Instant.now();
         timeProvider.setTime(startTime);
-        var limited = mailCounter.isLimited();
 
-        assertThat(limited)
+        assertThat(mailCounter.isLimited())
             .isFalse();
 
+        var limited = false;
         for (int i = 0; i < 100; i++) {
             limited = mailCounter.isLimited();
         }
-
         assertThat(limited)
             .isTrue();
 
         timeProvider.setTime(startTime.plus(1, ChronoUnit.HOURS));
 
-        limited = mailCounter.isLimited();
-        assertThat(limited)
+        assertThat(mailCounter.isLimited())
             .isFalse();
     }
 }
